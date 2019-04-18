d0998df67ac82e4175eef9550853ce682c8d3f4a
fut0k0 <cccabell@icloud.com>
2019-04-18 15:59:17 -0700

    Passing lab

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -3,7 +3,7 @@ def second_supply_for_fourth_of_july(holiday_hash)
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
-  holiday_hash[:winter].each {|holiday, item| item.push(supply)}
+  holiday_hash[:winter].each {|holiday, items| items.push(supply)}
 end
 
 def add_supply_to_memorial_day(holiday_hash, supply)
@@ -19,20 +19,20 @@ def all_winter_holiday_supplies(holiday_hash)
 end
 
 def all_supplies_in_holidays(holiday_hash)
-  holiday_hash.each do |holiday, list|
-    puts "#{holiday.to_s.capitalize}:"
-    list.each do |title, set|
-      title_frm = title.to_s.split("_").each {|item| item.capitalize!} .join(" ")
-      puts "  #{title_frm}: #{set.join(", ")}"
+  holiday_hash.each do |season, list|
+    puts "#{season.to_s.capitalize}:"
+    list.each do |holiday, items|
+      holiday_frm = holiday.to_s.split("_").each {|word| word.capitalize!} .join(" ")
+      puts "  #{holiday_frm}: #{items.join(", ")}"
     end
   end
 end
 
 def all_holidays_with_bbq(holiday_hash)
   bbq = []
-  holiday_hash.each do |holiday, list|
-    list.each do |title, set|
-      bbq.push(title) if set.include?("BBQ")
+  holiday_hash.each do |season, list|
+    list.each do |holiday, items|
+      bbq.push(holiday) if items.include?("BBQ")
     end
   end
   bbq
